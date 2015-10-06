5c00ccb1068b099f972d86702e5befdfb34dd596
aadame3311 <fps3311@gmail.com>
2015-10-06 16:32:51 -0500

    Added movement to the player

---
+++ SimpleFightingGame/Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 pos = transform.position;
+		pos.x += Input.GetAxis ("Horizontal");
+		transform.position = pos;
+
+		Vector2 pos2 = transform.position;
+		pos.y += Input.GetAxis ("Vertical");
+		transform.position = pos2;
+		}
+}
