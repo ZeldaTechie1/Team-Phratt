a39ec5449b432ccad598b71aa6f20836f131e5e7
FacelessTerror <bekko.legendary@hotmail.com>
2015-10-06 23:05:24 -0500

    Added more to the Health System

--- SimpleFightingGame/Assets/HealthBar.cs
+++ SimpleFightingGame/Assets/HealthBar.cs
@@ -4,16 +4,30 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour {
 
+    public float Health = 100f;
+    public float maxHealth = 100f;
+    public float minHealth = 0f;
+    Image image;
 
-
-
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+	Image image = GetComponent<Image>();
+         
 	}
+
+    void Damage(float value)
+    {
+        Health = Health - value;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Image image = GetComponent<Image>();
+         image.fillAmount = Health;
+
+          if (Health < minHealth)
+            { Health = minHealth; }
+          if(Health > maxHealth)
+            { Health = maxHealth; }
+
     }
 }
